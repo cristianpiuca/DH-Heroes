df094e1a968ac80b913f6eb41fd9ee3cac5f0b5b
cristianpiuca <cristianalexanderp1995@gmail.com>
2022-03-11 16:29:51 -0300

    trabajo dh heroes

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require ("express");
+const app = express();
+const port = 3030;
+const path = require ("path");
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req,res) => {
+    return res.sendFile(path.join(__dirname, "views", "home.html"))
+});
+
+app.get("/babbage", (req,res) => {
+    return res.sendFile(path.join(__dirname, "views", "babbage.html"))
+});
+
+app.get("/berners-lee", (req,res) => {
+    return res.sendFile(path.join(__dirname, "views", "berners-lee.html"))
+});
+
+app.get("/clarke", (req,res) => {
+    return res.sendFile(path.join(__dirname, "views", "clarke.html"))
+});
+
+app.get("/hamilton", (req,res) => {
+    return res.sendFile(path.join(__dirname, "views", "hamilton.html"))
+});
+
+app.get("/hopper", (req,res) => {
+    return res.sendFile(path.join(__dirname, "views", "hopper.html"))
+});
+
+app.get("/lovelace", (req,res) => {
+    return res.sendFile(path.join(__dirname, "views", "lovelace.html"))
+});
+
+app.get("/turing", (req,res) => {
+    return res.sendFile(path.join(__dirname, "views", "turing.html"))
+});
+
+app.get("*", (req,res) => {
+    return res.sendFile(path.join(__dirname, "views", "notFound.html"))
+});
+
+app.listen(port, () => console.log(`Servidor corriendo en el puerto 3030 http://localhost:${port}`))
